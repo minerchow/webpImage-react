f27c4133ea66f3fa591e15577f1b9401e3593078
zhouming <zhouming@gaodun.com>
2018-11-07 18:39:49 +0800

    解决异步数据

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import WebpImage from 'webpimagereact/lib/webpImage';
+// import WebpImage from 'webpimagereact/lib/webpImage';
+import WebpImage from '../components/index/webpImage'
 class Index extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            imgUrl:''
+        }
        
+    }
+    componentDidMount(){    
+        setTimeout(() => {
+            this.setState({
+                imgUrl:'http://image-demo.oss-cn-hangzhou.aliyuncs.com/example.jpg?x-oss-process=image/resize,m_fixed,h_100,w_100'
+            })
+        }, 2000);
     }
     render(){
         return <div>
-            <WebpImage className="a" source="http://image-demo.oss-cn-hangzhou.aliyuncs.com/example.jpg?x-oss-process=image/resize,m_fixed,h_100,w_100"></WebpImage>
+            <WebpImage className="a" source={this.state.imgUrl}></WebpImage>
         </div>
     }
 
--- src/components/index/webpImage.jsx
+++ src/components/index/webpImage.jsx
@@ -4,29 +4,48 @@ import { render } from 'react-dom';
 class WebpImage extends Component {
     constructor(props){
         super(props);
-        console.log(this.props)
+       // console.log(this.props)
         this.state = {
-            source : props.source
+            source : ''
         }
     }
     render(){
-        return (
-            <img src={this.state.source} width={this.props.width} height={this.props.height} className={this.props.className}/>
-            
-        );
+        console.log(this.state.source)
+        let imgSource;
+        if(this.state.source){
+            console.log(11111)
+            imgSource = <img src={this.state.source} width={this.props.width} height={this.props.height} className={this.props.className}/>
+        }else{
+            console.log(222222)
+            imgSource = ''
+        }
+        return <div>{imgSource}</div>
+        
+    }
+    componentWillReceiveProps(nextProps) {
+        // ...
+        console.log(nextProps)
+        if(nextProps){
+            this.convertWebP(nextProps)
+        }
+        
     }
     componentDidMount(){
-      
+        console.log(this.props)
+        this.convertWebP(this.props)
+       
+    }
+    convertWebP(nextProps){
         var canvas = document.createElement('canvas');
         if (Boolean(canvas.getContext && canvas.getContext('2d'))) {
-            if (canvas.toDataURL('image/webp').indexOf('data:image/webp') > -1) {
-                if(this.state.source.indexOf('?x-oss-process=image')>-1){
+            if (canvas.toDataURL('image/webp').indexOf('data:image/webp') > -1 && nextProps.source) {
+                if(nextProps.source.indexOf('?x-oss-process=image')>-1){
                     this.setState({
-                        source : this.state.source + "/format,webp"
+                        source : nextProps.source + "/format,webp"
                     })
                 }else{
                     this.setState({
-                        source : this.state.source + "?x-oss-process=image/format,webp"
+                        source : nextProps.source + "?x-oss-process=image/format,webp"
                     })
                 }
                 
